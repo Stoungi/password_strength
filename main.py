2bbed3c1dba045deb28259b01370d7d97ad5c860
Stoungi <36931144+Stoungi@users.noreply.github.com>
2024-06-30 07:44:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from ManageData import PassTrain
+
+password = input("give a password to score\n~>>")
+print("processing please wait")
+data = PassTrain("processed_data.csv")
+
+score = data.score_password(password)
+
+
+if score == 0:
+    print("your password is pretty bad, here is how you can improve it:")
+    print(data.suggestImprovements(password))
+if score == 1:
+    print("your password is good, but can do better:")
+    print(data.suggestImprovements(password))
+if score == 2:
+    print(data.suggestImprovements(password))
