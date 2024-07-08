71f56199aecdcaad176ebb3bfa7f544d48bb3ac5
Stoungi <36931144+Stoungi@users.noreply.github.com>
2024-07-08 17:46:16 +0300

    Update main.py
    here is a showcase of the history feature

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from ManageData import PassTrain
 
 
-password = input("give a password to score\n~>>")
-print("processing please wait")
-data = PassTrain("processed_data.csv", password)
-
-# note if you wanted to use it how it was before when you do data.score_passowrd(<input>, do_guess = True)
+print("please wait a moment...")
+data = PassTrain("processed_data.csv")
 
 
-if data.score == 0: 
-    print("your password is pretty bad, here is how you can improve it:")
-    print(data.suggestImprovements(data.improve))
-if data.score == 1:
-    print("your password is good, but can do better:")
-    print(data.suggestImprovements(data.improve))
-if data.score == 2:
-    print(data.suggestImprovements(data.improve))
 
-print(f"Overall time: {PassTrain.overall_time:.2f} seconds")
+while True:
+
+    choice = int(input("what would you like to do?\n 1. rate password\n 2. history\n 3. exist\n~>>"))
+
+    if choice == 3:
+        break
+    elif choice == 2:
+        for password, improve in data.history:
+            print(f"{password}\n{improve}")
+    elif choice == 1:
+        data.score_password(input("give a password to score\n~>>"))
+        if data.score == 0:
+            print("your password is pretty bad, here is how you can improve it:")
+            print(data.improve)
+        if data.score == 1:
+            print("your password is good, but can do better:")
+            print(data.improve)
+        if data.score == 2:
+            print(data.improve)
+
+
+    else:
+        pass
