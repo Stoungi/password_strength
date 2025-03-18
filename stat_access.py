1e80303b5289b0452ff570c0e936eff7728f14e1
Stoungi <36931144+Stoungi@users.noreply.github.com>
2025-03-18 14:51:31 +0000

    Update stat_access.py

--- stat_access.py
+++ stat_access.py
@@ -4,17 +4,17 @@ from ManageData import PassTrain
 print("please wait a moment...")
 data = PassTrain("processed_data.csv")
 
-print(data.countBy(**{data._strength : 0}))
-print(data.countBy(**{data._strength : 1}))
-print(data.countBy(**{data._strength : 2}))
+print(data.countBy(**{"strength" : 0}))
+print(data.countBy(**{"strength"  : 1}))
+print(data.countBy(**{"strength" : 2}))
 
 
 
-print(data.getBy(**{data._total_len : 4}))
-print(data.countBy(**{data._total_len : 4}))
+print(data.getBy(**{"total_len" : 4}))
+print(data.countBy(**{ "total_len" : 4}))
 
 
-print(data.getBy(**{data._total_len : 4, data._upper_case_count : 2}))
-print(data.getBy(**{data._total_len : 4, data._upper_case_count : 1, data._lower_case_count : 2}))
-print(data.countBy(**{data._total_len : 4, data._upper_case_count : 2}))
-print(data.countBy(**{data._total_len : 4, data._upper_case_count : 1, data._lower_case_count : 2}))
+print(data.getBy(**{"total_len" : 4, "upper_case_count" : 2}))
+print(data.getBy(**{"total_len": 4, "upper_case_count" : 1, "lower_case_count" : 2}))
+print(data.countBy(**{"total_len": 4, "upper_case_count" : 2}))
+print(data.countBy(**{"total_len" : 4, "upper_case_count" : 1, "lower_case_count" : 2}))
